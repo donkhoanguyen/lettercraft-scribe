1bef112b3dbc029ac1d5c910b5f24e31ffefd4eb
Don Nguyen <122437174+donkhoanguyen@users.noreply.github.com>
2024-12-29 23:24:45 -0500

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -3,19 +3,17 @@ import Letter from '../components/Letter';
 
 const Index = () => {
   const [letterContent1] = useState({
-    text: "Chào Ngọc Mai,\n\nLời đầu tiên thì chắc chắn là chúc mừng sinh nhật em nha!\n\nMặc dù anh biết là có 1 tỉ cái ảnh, cái video của em xinh hơn, nhưng anh xin phép mở đầu với một trong những video anh thấy em cười tươi nhất. Em giỏi em đẹp em cố gắng, chúng ta đều đã khen nhiều lắm rồi. Hãy xem em cười tươi roi rói đây nhé!",
+    text: "Chào Ngọc Mai,\n\nLời đầu tiên chắc chắn là chúc mừng sinh nhật em nha! Anh nhớ em điên mẹ lên mất.\n\nMặc dù anh biết là có 1 tỉ cái ảnh, cái video của em xinh hơn, nhưng anh xin phép mở đầu với một trong những video anh thấy em cười tươi nhất. Em giỏi em đẹp em cố gắng, chúng ta đều đã khen nhiều lắm rồi. Hãy xem em cười tươi roi rói đây nhé!",
     mediaUrl: "/IMG_8202.mp4", // Replace with your .mp4 video once converted
     mediaType: "video" as const,
-    mediaAlt: "Mai Xinh",
-    overlayText: "Mai rat xinh"
+    mediaAlt: "Mai Xinh"
   });
 
   const [letterContent2] = useState({
-    text: "Chào Ngọc Mai,\n\nAnh nhớ em siêu nhiều luôn.\n\nTrong những ngày em đi thì anh có xem lại những video trước đây anh có với em. Anh thấy em trẻ trung xinh đẹp và thật đáng yêu, và mỗi lần nhìn thấy em xinh đẹp như vậy anh lại thấy ngạo nghễ hơn một chút\n\nBest regards,\nYour Name",
-    mediaUrl: "/IMG_8202.mp4", // Replace with your .mp4 video once converted
-    mediaType: "video" as const,
-    mediaAlt: "Mai Xinh",
-    overlayText: "Mai rat xinh"
+    text: "Nhân dịp sinh nhật em thì anh muốn chúc em hạnh phúc hơn, vui vẻ hơn, và với sự tài giỏi và consistent của em thì mọi thành công em mong muốn sẽ sớm đến. Anh hân hạnh được có mặt trong hành trình này cùng em, và khi em về anh sẽ có steak và pasta và tiramisu ngon nhất trên đời cho em.\n\nBest,\n\nYêu Ngọc Mai",
+    mediaUrl: "/IMG_9932.JPG", // Replace with your .mp4 video once converted
+    mediaType: "image" as const,
+    mediaAlt: "Mai Xinh"
   });
 
 
