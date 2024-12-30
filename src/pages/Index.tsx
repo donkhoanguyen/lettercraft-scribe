8f1f37a5f71a9a292f43f6c839b398478aa1ba28
Don Nguyen <122437174+donkhoanguyen@users.noreply.github.com>
2024-12-29 22:55:13 -0500

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -2,7 +2,15 @@ import { useState } from 'react';
 import Letter from '../components/Letter';
 
 const Index = () => {
-  const [letterContent] = useState({
+  const [letterContent1] = useState({
+    text: "Chào Ngọc Mai,\n\nAnh nhớ em siêu nhiều luôn.\n\nTrong những ngày em đi thì anh có xem lại những video trước đây anh có với em. Anh thấy em trẻ trung xinh đẹp và thật đáng yêu, và mỗi lần nhìn thấy em xinh đẹp như vậy anh lại thấy ngạo nghễ hơn một chút\n\nBest regards,\nYour Name",
+    mediaUrl: "/IMG_8202.mp4", // Replace with your .mp4 video once converted
+    mediaType: "video" as const,
+    mediaAlt: "Mai Xinh",
+    overlayText: "Mai rat xinh"
+  });
+
+  const [letterContent2] = useState({
     text: "Chào Ngọc Mai,\n\nAnh nhớ em siêu nhiều luôn.\n\nTrong những ngày em đi thì anh có xem lại những video trước đây anh có với em. Anh thấy em trẻ trung xinh đẹp và thật đáng yêu, và mỗi lần nhìn thấy em xinh đẹp như vậy anh lại thấy ngạo nghễ hơn một chút\n\nBest regards,\nYour Name",
     mediaUrl: "/IMG_8202.mp4", // Replace with your .mp4 video once converted
     mediaType: "video" as const,
@@ -12,7 +20,7 @@ const Index = () => {
 
   return (
     <div className="min-h-screen py-12 px-4">
-      <Letter content={letterContent} />
+      <Letter content={letterContent1, letterContent2} />
     </div>
   );
 };
